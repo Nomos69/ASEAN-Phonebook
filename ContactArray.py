65850b78af8fb85bd50a1fa0d8a040f8ca94b86b
Choosen_One <151849911+Nomos69@users.noreply.github.com>
2024-08-27 13:10:16 +0000

    methods in contactarray completed

--- ContactArray.py
+++ ContactArray.py
@@ -24,7 +24,9 @@ class ContactList:
             Returns none if the list is empty.
         """
         # Complete this Method
-        return 
+        if self.isEmpty():
+            return None
+        return self.phonebook[0]
     
     def getLast(self) -> Contact:
         """
@@ -32,7 +34,10 @@ class ContactList:
             Returns none if the list is empty.
         """
         # Complete this Method
-        return 
+        if self.isEmpty():
+            return None
+        return self.phonebook[self.size - 1]
+
 
     def getContactAtIndex(self, index: int) -> Contact:
         """Gets the contact at given index in the contact linked list.
@@ -45,7 +50,9 @@ class ContactList:
             Contact: Contact at index.
         """
         # Complete this Method
-        return 
+        if index < 0 or index >= self.size:
+            return None
+        return self.phonebook[index]
     
     def getContact(self, student_num: str) -> Contact:
         """Gets the contact based on given student number. Will return None
@@ -58,13 +65,19 @@ class ContactList:
             Contact: Contact information.
         """
         # Complete this Method
-        return 
+        for i in range(self.size):
+            if self.phonebook[i].student_num == student_num:
+                return self.phonebook[i]
+        return None
     
     def getContactBySurname(self, surname: str) -> Contact:
         """Gets the contact based on surname. Will return None if contact is not found.
         """
         # Complete this Method
-        return 
+        for i in range(self.size):
+            if self.phonebook[i].surname == surname:
+                return self.phonebook[i]
+        return None
     
     def isEmpty(self) -> bool:
         """
@@ -90,8 +103,11 @@ class ContactList:
             Increases the size of this phonebook by two times.
         """
         # Complete this Method
-        return 
-    
+        new_phonebook = [None] * (len(self.phonebook) * 2)
+        for i in range(self.size):
+            new_phonebook[i] = self.phonebook[i]
+        self.phonebook = new_phonebook 
+
     def insert(self, c : Contact):
         """Inserts new contact at index point.
 
@@ -99,7 +115,12 @@ class ContactList:
             c (Contact): Contact to be inserted.
         """
         # Complete this Method
-        return 
+        if self.size == len(self.phonebook):
+            self.__increasePhonebookSize()
+        index = self.__findIndexInsertion(c)
+        self.__adjustPhonebook(index, self.size, "b")
+        self.phonebook[index] = c
+        self.incrSize() 
 
     def __findIndexInsertion(self, c: Contact) -> int:
         """Finds the index to insert from based on contact's
@@ -112,7 +133,13 @@ class ContactList:
             int: Node insertion point for new contact.
         """
         # Complete this Method
-        return 
+        for i in range(self.size):
+            if c.surname < self.phonebook[i].surname:
+                return i
+            elif c.surname == self.phonebook[i].surname and c.firstname < self.phonebook[i].firstname:
+                return i
+        return self.size
+    
     
     def __adjustPhonebook(self, start: int, end = int, dir: str = "f") -> None:
         """
@@ -125,7 +152,12 @@ class ContactList:
                 "b" -> Backward indexing adjustment. For example, elemet at index 1 takes the value of the element at index 0.
         """
         # Complete this Method
-        return 
+        if dir == "f":
+            for i in range(start, end):
+                self.phonebook[i] = self.phonebook[i + 1]
+        elif dir == "b":
+            for i in range(start + 1 , end):
+                self.phonebook[i] = self.phonebook[i - 1]
     
     def deleteContact(self, stdn: str) -> Contact:
         """Finds the contact based on their student number.
@@ -138,7 +170,13 @@ class ContactList:
             Contact: Deleted contact, if found.
         """
         # Complete this Method
-        return 
+        for i in range(self.size):
+            if self.phonebook[i].student_num == stdn:
+                deleted_contact = self.phonebook[i]
+                self.__adjustPhonebook(i, self.size - 1, "f")
+                self.decrSize()
+                return deleted_contact
+        return None
         
     def __sort(self, by: str) -> list:
         """
@@ -152,7 +190,15 @@ class ContactList:
         """
         # Complete this method optionally
         # Correctly completing this method will reward up to 4 bonus points in your MTE.
-        return -1
+
+        sorted_phonebook = self.phonebook[:self.size]
+        if by == 'lname':
+            sorted_phonebook.sort(key=lambda x: x.surname)
+        elif by == 'fname':
+            sorted_phonebook.sort(key=lambda x: x.firstname)
+        elif by == 'stdn':
+            sorted_phonebook.sort(key=lambda x: x.student_num)
+        return sorted_phonebook
         
     def __str__(self, f = None, by: str = 'lname') -> str:
         """
@@ -172,7 +218,10 @@ class ContactList:
         if not self.isEmpty():
             # Complete this method.
             # Optionally include viewing contacts in a particular order for up to 1 bonus point in your MTE.
-            print("Not Empty")
+            sorted_phonebook = self.__sort(by)
+        for contact in sorted_phonebook:
+            if f is None or contact.country_code in f:
+                s += str(contact) + "\n"
         else:
             s += "\nThis phonebook is currently empty..."
         s += "\n<----End---->"
